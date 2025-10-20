9d646980a71c50a1875e172e5860966fa360a7ee
Mark <markjohnguto@gmail.com>
2025-10-20 12:04:05 +0300

    modified

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import"./PropertyCard.css"
 
-export default function PropertyCard({ property }) {
-    const { image, title, location, price, bedrooms, bathrooms } = property;
-
+export default function PropertyCard() {
+   
 
   return (
-      <div className="propety-card">
-          <img src={image} alt={title} className="property-image" />
-          
-          <div className="property-info">
-              <h3>{title}</h3>
-              <p className="property-location">{location}</p>
-              <p className="property-price">${price.toLocaleString()}</p>
+    <div className="property-card">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlu6DGo9rD_5Ypmuctli8uge_VwDRVpJvC1g&s"
+        alt="property image"
+        className="property-image"
+      />
 
-              <div className="property-details">
-                  <span>{bedrooms}</span>
-                  <span>{bathrooms}</span>
-              </div>
-          </div>
+      <div className="property-info">
+        <h3>Olive Properies</h3>
+        <p className="property-location">Nairobi</p>
+        <p className="property-price">$70,000</p>
+
+        <div className="property-details">
+          <span>4 bedrooms</span>
+          <div></div>
+          <span>3 bathrooms </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
