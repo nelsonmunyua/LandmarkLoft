404c68c58d07a46172ed9ccd76199070c62d78c8
Esther nekesa <esthernekesasi32@gmail.com>
2025-10-21 10:49:41 +0300

    add my index.css and PropertyList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import PropertyDetails from './components/propertydetails/PropertyDetails'
+import React from "react";
+import PropertyDetails from "./components/propertydetails/PropertyDetails";
 import Navbar from "./components/navbar/Navbar";
-import Filter from './components/filter/filter';
+import Filter from "./components/filter/filter";
+import PropertyList from "./components/propertylist/PropertyList";
 
 function App() {
   return (
     <>
-    <Filter />
-    <Navbar/>
+      <Filter />
+      <Navbar />
       <PropertyDetails />
+      <PropertyList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
